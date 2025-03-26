e113b1f2a8bb3a58714d0407c1dbd43402551799
EnigmaticJihad <enigmatic.jihad@gmail.com>
2025-03-26 20:50:42 +0600

    initial commit

--- randwal.sh
+++ randwal.sh
@@ -1,1 +1,20 @@
-/home/zed/.randwal
+#!/bin/sh
+
+# Directory containing the wallpapers
+wallpapers_dir="/home/zed/Pictures/gowall/"
+
+# Function to set the wallpaper
+set_wallpaper() {
+  local random_image=$(ls -p "$wallpapers_dir" | shuf -n 1)
+  feh --bg-fill "$wallpapers_dir/$random_image" &
+}
+
+# Set the initial wallpaper
+set_wallpaper
+
+# Loop to change wallpaper every 10 seconds
+while true; do
+  sleep 60
+  set_wallpaper
+done
+
